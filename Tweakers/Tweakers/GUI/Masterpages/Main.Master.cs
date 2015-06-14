7a84386537139cb81c293b97be2d3b3689d02051
LaurensAdema <laurensadema@live.nl>
2015-06-14 23:06:54 +0200

    Optimalisatie GitHub
    Verwijderen van onnodige referenties

--- Tweakers/Tweakers/GUI/Masterpages/Main.Master.cs
+++ Tweakers/Tweakers/GUI/Masterpages/Main.Master.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Optimization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
